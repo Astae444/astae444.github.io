6bdfee750807190befd0c0ff763c1b818ad7b5db
Astae444 <mathiscrochet@gmail.com>
2024-11-25 12:07:07 +0100

    Ajustements

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Config
-{
-    const SERVEUR = "sql113.epizy.com";
-    const BDD = "epiz_33766799_db_portfolio";
-    const UTILISATEUR = "epiz_33766799";
-    const MOTDEPASSE = "wzrcWV8McjIe";
-}
